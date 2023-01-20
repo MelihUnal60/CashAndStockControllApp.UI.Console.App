c3a2d6682a404df2a82a8d8d78f551ac5dc0da7a
MelihUnal60 <111892158+MelihUnal60@users.noreply.github.com>
2023-01-20 17:02:51 +0300

    Log updates

--- CashAndStockControllApp.UI.Console.App/Program.cs
+++ CashAndStockControllApp.UI.Console.App/Program.cs
@@ -9,39 +9,42 @@ static void Menu()
 {
     try
     {
-        var choose = TakeAnswer("1. Add İtem\n2. Sell İtem\n3. Cash Info\n4. Exit\n5. Error logs\n6.Warning logs\n7.Info logs", false);
-        switch (choose)
+        while (true)
         {
-            case "0":
-                throw new Exception("Bu değeri asla girme!");
-            case "1":
-                AddItem();
-                break;
-            case "2":
-                SellItem();
-                break;
-            case "3":
-                CashInfo();
-                break;
-            case "4":
-                Environment.Exit(0);
-                break;
-            case "5":
-                LogService.ShowErrorLogList();
-                break;
-            case "6":
-                LogService.ShowWarningLogList();
-                break;
-            case "7":
-                LogService.ShowInfoLogList();
-                break;
-                
-            default:
-                //Log warning = new Log(LogType.Warning, "Lütfen belirtilen değerlerden birini giriniz!!");
-                //Console.WriteLine(warning.ToString());
-                LogService.WarningLog("Lütfen belirtilen değerlerden birini giriniz!!");
-                ReturnToMenu();
-                break;
+            var choose = TakeAnswer("1. Add İtem\n2. Sell İtem\n3. Cash Info\n4. Exit\n5. Error logs\n6.Warning logs\n7.Info logs", false);
+            switch (choose)
+            {
+                case "0":
+                    throw new Exception("Bu değeri asla girme!");
+                case "1":
+                    AddItem();
+                    break;
+                case "2":
+                    SellItem();
+                    break;
+                case "3":
+                    CashInfo();
+                    break;
+                case "4":
+                    Environment.Exit(0);
+                    break;
+                case "5":
+                    LogService.ShowErrorLogList();
+                    break;
+                case "6":
+                    LogService.ShowWarningLogList();
+                    break;
+                case "7":
+                    LogService.ShowInfoLogList();
+                    break;
+
+                default:
+                    //Log warning = new Log(LogType.Warning, "Lütfen belirtilen değerlerden birini giriniz!!");
+                    //Console.WriteLine(warning.ToString());
+                    LogService.WarningLog("Lütfen belirtilen değerlerden birini giriniz!!");
+                    //ReturnToMenu();
+                    break;
+            }
         }
     }
     catch(Exception err)
@@ -60,17 +63,17 @@ static void ReturnToMenu(string message = "Menüye dönmek için ENTER")
 
 static void CashInfo()
 {
-    throw new NotImplementedException();
+    
 }
 
 static void SellItem()
 {
-    throw new NotImplementedException();
+    
 }
 
 static void AddItem()
 {
-    throw new NotImplementedException();
+    
 }
 
 static string TakeAnswer(string screenText,bool sameRow = true)
